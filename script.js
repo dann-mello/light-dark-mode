9136c954e22c1971864953ecae93b225b968476a
Daniel Melo <o.danielworkmelo@gmail.com>
2023-02-20 17:04:49 +0000

    Refatorado

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-let btn = document.querySelector('button');
-let title = document.querySelector('.container h1');
+let btn = document.querySelector('#mode-selector');
+let title = document.querySelector('#page-title');
 let footer = document.querySelector('footer');
-let font_footer = document.querySelector('.align');
-let main = document.querySelector('main');
+let body = document.querySelector('body');
+let darkModeClass = 'dark-mode'
 
-btn.addEventListener('click', () => {
-    if(btn.textContent === 'Dark Mode'){
-        main.style.backgroundColor = 'black';
-        title.textContent = 'Dark Mode On';
-        title.style.color = 'white';
-        btn.style.backgroundColor = 'white';
-        btn.style.color = 'black'; 
-        btn.classList.add('ligth');
-        btn.classList.remove('dark');
-        btn.textContent = 'Light Mode'
-        footer.style.backgroundColor = '#333'
-        font_footer.style.color = 'black';
-    }else{
-        main.style.backgroundColor = 'white';
-        title.textContent = 'Light Mode On';
-        title.style.color = 'black';
-        btn.style.backgroundColor = '#5a5a5a';
-        btn.style.color = 'white'; 
-        btn.classList.add('dark');
-        btn.classList.remove('ligth');
-        btn.textContent = 'Dark Mode'
-        footer.style.backgroundColor = '#5a5a5a'
+function changeMode() {
+    changeClasses();
+    changeText();
+}
+
+function changeText() {
+    const LIGHT_MODE = 'Light Mode';
+    const DARK_MODE = 'Dark Mode';
+    if (body.classList.contains(darkModeClass)) {
+        btn.textContent = LIGHT_MODE;
+        title.textContent = LIGHT_MODE + ' ON';
+        return
     }
-})
+
+    btn.textContent = DARK_MODE;
+    title.textContent = DARK_MODE + ' ON';
+}
+
+function changeClasses() {
+    btn.classList.toggle(darkModeClass);
+    title.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+}
+
+btn.addEventListener('click', changeMode);
